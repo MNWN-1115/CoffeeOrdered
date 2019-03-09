84662e717342820c81cba7270e5a078550deea91
Dakaney <wayman_ng@yahoo.com>
2019-03-09 14:36:44 -0800

    built structure for listview

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import ListViewItem from './ListViewItem';
+import fakeData from '../Dummydata/fakeData';
 
 class ListView extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            currentOrders: fakeData
+        }
+    }
+
     render () {
         return (
-            <div>ListView</div>
+            <div>{this.state.currentOrders.map(item => <ListViewItem item={item} />)}</div>
         ) 
     }
 }
--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -3,10 +3,18 @@ import ListView from './ListView';
 
 const OrderList = () => {
     return (
-        <div>
-            <div>
-                <ListView />
+        <div class="orderList">
+            <h3>ORDERS</h3>
+            <div class="ListHeader">
+                <p>Coffee</p>
+                <p>Method</p>
+                <p># of Cases</p>
+                <p>Packets per Case</p>
+                <p>Ship Date</p>
+                <p>star</p>
+                <p>Order</p>
             </div>
+            <ListView />
         </div>
     );
 }
